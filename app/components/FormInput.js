46eb9b00b67367973c0ec093847edecce5869787
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-04-06 20:29:15 -0400

    Add validation ith Formik and Yup

--- app/components/FormInput.js
+++ app/components/FormInput.js
@@ -2,11 +2,14 @@ import { View, Text, TextInput } from 'react-native';
 import React from 'react';
 
 const FormInput = (props) => {
-  const { label, placeholder } = props;
+  const { label, placeholder, error } = props;
 
   return (
     <>
-      <Text style={{ fontWeight: 'bold', }}>{label}</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }} >
+        <Text style={{ fontWeight: 'bold' }}>{label}</Text>
+        {error ? <Text style={{ color: 'red', fontSize: 16, marginBottom: 5 }}>{error}</Text> : null}
+      </View>
       <TextInput {...props} placeholder={placeholder} style={styles.input} />
     </>
   )
